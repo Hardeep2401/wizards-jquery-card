d8841ee7a5744dc46145ce193271758d19023618
Hardeep2401 <hardeep.singh@lpinfotech.com>
2022-12-08 17:29:16 +0530

    css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 $(document).ready(function () {
-
-
     $(".card-first").click(function () {
         $(this).siblings().removeClass('active');
         $(this).toggleClass("active");
         $("#next").removeAttr('disabled');
     });
-
     $("#next").click(function () {
         $("#barFirst").css({ 'border-color': 'green', 'background-color': 'yellow' })
     });
