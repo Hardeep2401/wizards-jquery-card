9acddc1bb0fb6809896028a697ca62893791eb6d
Hardeep2401 <hardeep.singh@lpinfotech.com>
2022-12-09 10:12:57 +0530

    add value

--- js/main.js
+++ js/main.js
@@ -6,8 +6,19 @@ $(document).ready(function () {
     });
     $("#next").click(function () {
         $("#barFirst").css({ 'border-color': 'green', 'background-color': 'yellow' });
+
     });
-    
+    $("#next").click(function () {
+        $("#barSecond").css({ 'border-color': 'green', 'background-color': 'yellow' });
+        $("#card-1").fadeOut();
+        $("#card-2").fadeIn();
+    });
+    $("#next").click(function () {
+        $("#barThree").css({ 'border-color': 'green', 'background-color': 'yellow' });
+        $("#card-1").fadeOut();
+        $("#card-2").fadeOut();
+        $("#card-3").fadeIn();
+    });
 });
 
 
