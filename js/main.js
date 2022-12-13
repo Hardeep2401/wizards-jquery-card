eb9dec09b32abd2e8e16369c9dc9afcc0b3b3d5a
Hardeep2401 <hardeep.singh@lpinfotech.com>
2022-12-13 11:48:19 +0530

    updated with on click button

--- js/main.js
+++ js/main.js
@@ -2,40 +2,45 @@ $(document).ready(function () {
     $(".card-first-1").click(function () {
         $(this).siblings().removeClass('active');
         $(this).toggleClass("active");
-        $("#next").removeAttr('disabled');
+        $("#next-1").removeAttr('disabled');
     });
 
-    $("#next").click(function () {
+    $("#next-1").click(function () {
         $("#barFirst").css({ 'border-color': 'green', 'background-color': 'yellow' });
         $("#card-1").css('display', 'none');
         $("#card-1").fadeOut();
         $("#card-2").fadeIn();
-        $("#next").attr("disabled",);
+
 
     });
 
     $(".card-first-2").click(function () {
-
         $(this).siblings().removeClass('active');
         $(this).toggleClass("active");
+        $("#next-2").removeAttr('disabled');
+    });
+    $("#next-2").click(function () {
+
+        $("#barSecond").css({ 'border-color': 'green', 'background-color': 'yellow' });
         $("#card-2").css('display', 'none');
         $("#card-2").fadeOut();
         $("#card-3").fadeIn();
-
-        $("#barSecond").css({ 'border-color': 'green', 'background-color': 'yellow' });
-        $("#next").removeAttr('disabled');
     });
-
-
     $(".card-first-3").click(function () {
-
         $(this).siblings().removeClass('active');
         $(this).toggleClass("active");
+        $("#next-3").removeAttr('disabled');
+    });
+    $("#next-3").click(function () {
+        $("#barThree").css({ 'border-color': 'green', 'background-color': 'yellow' });
         $("#card-3").css('display', 'none');
         $("#card-3").fadeOut();
-        $("#card-4").fadeIn();
-        $("#barThree").css({ 'border-color': 'green', 'background-color': 'yellow' });
-        $("#next").removeAttr('disabled');
+        $("#card-4").fadeIn().css();
+    });
+    $(".card-first-4").click(function () {
+        $(this).siblings().removeClass('active');
+        $(this).toggleClass("active");
+        $("#next-3").removeAttr('disabled');
     });
 
 
